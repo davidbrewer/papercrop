665fa37b9bac0359e216e403c67f8fd9bd605ad1
David Brewer <dbrewer@secondstory.com>
2015-04-17 15:02:33 -0700

    Added support for a "set_select" passthrough option in cropbox helper.

--- lib/papercrop/helpers.rb
+++ lib/papercrop/helpers.rb
@@ -31,8 +31,14 @@ module Papercrop
     # Form helper to render the main cropping box of an attachment.
     # Loads the original image. Initially the cropbox has no limits on dimensions, showing the image at full size.
     # You can restrict it by setting the :width option to the width you want.
+    # You can choose whether or not a selection is defined by default using the option
+    # :set_select with the following possible values:
+    # 'minimum': use the minimum possible selection, centered in the image
+    # 'maximum': use the maximum possible selection, centered in the image
+    # 'x,y,x2,y2': use a selection defined by its top left and bottom right corner
+    # nil or false: don't start out with a selection
     #
-    #   cropbox :avatar, :width => 650
+    #   cropbox :avatar, :width => 650, :set_select => 'minimum'
     #
     # @param attachment [Symbol] attachment name
     # @param opts [Hash]
@@ -41,11 +47,13 @@ module Papercrop
       original_width  = self.object.image_geometry(attachment, :original).width
       original_height = self.object.image_geometry(attachment, :original).height
       box_width       = opts[:width] || original_width
+      set_select      = opts[:set_select] || nil
 
       if self.object.send(attachment).class == Paperclip::Attachment
         box  = self.hidden_field(:"#{attachment}_original_w", :value => original_width)
         box << self.hidden_field(:"#{attachment}_original_h", :value => original_height)
         box << self.hidden_field(:"#{attachment}_box_w",      :value => box_width)
+        box << self.hidden_field(:"#{attachment}_set_select", :value => set_select)
 
         for attribute in [:crop_x, :crop_y, :crop_w, :crop_h, :aspect, :min_size, :max_size] do
           box << self.hidden_field(:"#{attachment}_#{attribute}", :id => "#{attachment}_#{attribute}")
